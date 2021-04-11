361b5f52ed1b333cf02ef238b09cc681ab5c5d36
Diogo <diogoppedro@gmail.com>
2021-04-11 16:08:22 +0400

    phase 1 commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ const About = ({ data }) => {
           <img
             className="profile-pic"
             src={profilepic}
-            alt="Sonny's Profile Pic"
+            alt="Diogo's Profile Pic"
           />
         </div>
         <div className="nine columns main-col">
@@ -48,7 +48,7 @@ const About = ({ data }) => {
             <div className="columns download">
               <p>
                 <a href={resumeDownload} className="button">
-                  <i className="fa fa-download"></i>Download Resume
+                  <i className="fa fa-download"></i>Check Out My Resume
                 </a>
               </p>
             </div>
